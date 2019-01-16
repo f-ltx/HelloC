14014cfff7e35cf5a6296e929905d9c210ee8f43
214186071 <214186071@qq.com>
2019-01-16 23:20:24 +0800

    第一章

--- Chapter01/HelloWorld.c
+++ Chapter01/HelloWorld.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
+void fun(void){
+    printf("Hello fun\n");
+}
+void fun2();                        //通知编译器，要用到这个函数
 int main(void){
-    printf("Hello , C Primer .");
-    printf("Hello , C Primer .");
+    printf("Hello , C Primer .\n");
+    fun();
+    fun2();                         //调用
+    printf("Hello , C Primer .\n");
     return 0;
+}
+void fun2(){                        //定义函数
+    printf("Hello fun2 \n");
 }
