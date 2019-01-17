5f5c5cf7fc60bf897f4fa37b2819529b369df9d5
f-ltx <214186071@qq.com>
2019-01-17 11:20:02 +0800

    C语言启航

--- Chapter01/HelloWorld.c
+++ Chapter01/HelloWorld.c
@@ -8,6 +8,17 @@ int main(void){
     fun();
     fun2();                         //调用
     printf("Hello , C Primer .\n");
+
+    //8进制、10进制、16进制
+    printf("========================\n");
+    int x=100;
+    printf("%d \n",x);
+    printf("%x \n",x);
+    printf("%o \n",x);
+    printf("%#x \n",x);
+    printf("%#o \n",x);
+
+
     return 0;
 }
 void fun2(){                        //定义函数
